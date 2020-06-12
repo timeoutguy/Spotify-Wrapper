2de7e81fadaac902a414f1280ee307fd6c9704b9
MARCUS VINICIUS SOUZA SILVA <marcus.silva58@fatec.sp.gov.br>
2020-06-12 08:09:47 -0300

    Config files (eslint, nodemon, package.json, yarn.lock, gitignore)

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "es2020": true,
+        "node": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
